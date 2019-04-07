209c005ccbd924174689002fae1c863f06c734c4
zzuuoom <18829071821@163.com>
2019-04-07 14:01:41 +0800

    挂号

---
+++ src/main/java/com/zuo/smartbackground/model/BookDocSche.java
@@ -0,0 +1,32 @@
+package com.zuo.smartbackground.model;
+
+
+public class BookDocSche {
+    private Book book;
+    private Schedule schedule;
+    private Doctor doctor;
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+}
